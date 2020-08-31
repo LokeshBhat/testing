1a24148be593b0e4159b9f1672d0cbfc590e4641
LokeshBhat <66522207+LokeshBhat@users.noreply.github.com>
2020-08-31 12:36:38 +0530

    Add files via upload

---
+++ 21aug.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 19:08:20 2020
+
+@author: Lokesh
+"""
+
+l1 = ['hi','how','are','you']
+
+myString = ' '.join(l1) #urdu
+#print (myString)
+
+l2 = myString.split(' ')
+#print(l2)
+
+l3 = [1,2,3,4,2] #duplicate eliment is present
+l4 = [8,5,6,7]
+
+#print(l4)
+
+#set datatype
+l3 = set(l3) # duplicate eliment gets eliminted in this process
+l4 = set(l4)
+
+#print(l3 & l4)
+
+h = "what a bright sunny day!" #couldn't sort it out
+
+#inside a join function you can not have function called
+n = 3
+l5 = []
+l6 = l5.remove('!')
+
+testList = h.split(' ')
+
+for i in testList:
+    if (len(i)>3):
+        l5.append(i)
+
+
+def div(num):
+    number = 5
+    #if ()
+
+
+#time complexity problem
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
